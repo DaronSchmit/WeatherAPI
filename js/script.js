8bff45aed1bf864f257d0388fd73ad5a8b0cd591
DaronS23 <darons23@gmail.com>
2020-11-06 10:33:25 -0600

    added onclick functionality

--- js/script.js
+++ js/script.js
@@ -17,15 +17,6 @@ if(currentCity === null){
   localStorage.setItem('storedCity', JSON.stringify(currentCity));
 }
 
-//testing city history and current city
-// currentCity = 'Minneapolis';
-// cityHistory = ['Houston','New York', 'Miami'];
-
-// localStorage.setItem('storedHistory', JSON.stringify(cityHistory) );
-// localStorage.setItem('storedCity', JSON.stringify(currentCity));
-
-
-
 //generate city history in sidebar
 
 let historyUL = $('#city-history');
@@ -39,64 +30,37 @@ for (let i = 0; i < cityHistory.length; i++){
 $('#current-conditions').append('<h4>Current conditions in '+currentCity+'</h4>');
 $('#current-forecast').append('<p><strong>5-Day forecast for '+currentCity+'</strong></p>');
 
-let queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=minneapolis&appid='+weatherKey;
+
+
+
+  
+//make button on click function to make api call
+
+//click takes input, makes API call using text input
 let currentConditions;
 
-$.ajax({
+$("#search-button").on('click',function(){
+  event.preventDefault();
+  let queryCity = $('input').val();
+  let queryURL = 'http://api.openweathermap.org/data/2.5/weather?q='+queryCity+'&appid='+weatherKey;
+  
+  $.ajax({
   url: queryURL,
   method: 'GET'
 })
   .then(function(response) {
     console.log(response);
-      currentConditions = response;
+    currentConditions = response;
+    currentCity = currentConditions.name;
+    let currentTemp = currentConditions.main.temp;
+    let currentHumidity = currentConditions.main.humidity;
+    let windSpeed = currentConditions.wind.speed;
+    cityHistory.push(currentCity);
+    
+    localStorage.setItem('storedHistory', JSON.stringify(cityHistory))
   });
+
   
   
-
-
-  let testObj = {
-  "coord": {
-    "lon": -122.08,
-    "lat": 37.39
-  },
-  "weather": [
-    {
-      "id": 800,
-      "main": "Clear",
-      "description": "clear sky",
-      "icon": "01d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 282.55,
-    "feels_like": 281.86,
-    "temp_min": 280.37,
-    "temp_max": 284.26,
-    "pressure": 1023,
-    "humidity": 100
-  },
-  "visibility": 16093,
-  "wind": {
-    "speed": 1.5,
-    "deg": 350
-  },
-  "clouds": {
-    "all": 1
-  },
-  "dt": 1560350645,
-  "sys": {
-    "type": 1,
-    "id": 5122,
-    "message": 0.0139,
-    "country": "US",
-    "sunrise": 1560343627,
-    "sunset": 1560396563
-  },
-  "timezone": -25200,
-  "id": 420006353,
-  "name": "Mountain View",
-  "cod": 200
-  }
-
-                
+  
+})
