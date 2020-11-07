253f2b8b6f26eddf2fe91b6035643906264f11e9
Daron Schmit <darons23@gmail.com>
2020-11-07 15:41:53 -0600

    got buttons working well

--- js/script.js
+++ js/script.js
@@ -26,10 +26,38 @@ else{
 //generate city history in sidebar
 
 let historyUL = $('#city-history');
-for (let i = 0; i < cityHistory.length; i++){
-  let newLI = $('<li id="city-in-history"></li>');
-  newLI.text(cityHistory[i]);
-  historyUL.append(newLI);
+updateHistoryUL(cityHistory);
+
+function updateHistoryUL(array){
+  //empty html element
+  historyUL.empty();
+  //reset and re-update button
+  for (let i = 0; i < array.length; i++){
+    //console.log(array[i]);
+    let buttonLabel = array[i];
+    let newLI = $('<li id="city-in-history"></li>');
+    let newButton = $('<button class="waves-effect waves-light btn sidebar-button" id="'+buttonLabel+'"></button');
+    newButton.text(buttonLabel);
+    newLI.append(newButton);
+    $(newButton).on('click', function(){
+      getWeatherUpdateHTML($(newButton).attr("id"))
+    });
+    historyUL.append(newLI);
+  }
+}
+
+function updateArray(array, newValue){
+  if(!array.includes(newValue)){
+    //put the item at the beginning of the array
+    array = [newValue].concat(array);
+  }
+  else{
+    let firstPart = array.slice(0,array.indexOf(newValue));
+    let lastPart = array.slice(array.indexOf(newValue)+1, array.length);
+    //remove the item and put it first again
+    array = [newValue].concat(firstPart, lastPart);
+  }
+  return array;
 }
 
 //generate Current City current weather
@@ -44,18 +72,10 @@ function kelvToFar(kelvin){
   return farenheit.toFixed(2);
 }
 
-  
-//make button on click function to make api call
-
-//click takes input, makes API call using text input
-let currentConditions;
-
-let queryCity = $('input').val();
-
 
 //make API call
-function getWeather(query){
-  let queryURL = 'http://api.openweathermap.org/data/2.5/weather?q='+queryCity+'&appid='+weatherKey;
+function getWeatherUpdateHTML(query){
+  let queryURL = 'http://api.openweathermap.org/data/2.5/weather?q='+query+'&appid='+weatherKey;
   $.ajax({
     url: queryURL,
     method: 'GET'
@@ -63,6 +83,27 @@ function getWeather(query){
     .then(function(response) {
       //get API information
       console.log(response);
+
+      currentCity = response.name;
+      
+      //get current weather information
+      let currentTemp = response.main.temp;
+      currentTemp = kelvToFar(currentTemp);
+      let currentHumidity = response.main.humidity;
+      let windSpeed = response.wind.speed*2.237;
+      
+      //save search as current city, save in local storage, and update html
+      localStorage.setItem('storedCity', JSON.stringify(currentCity));
+      $('#current-city').text(currentCity);
+      $('#temp').text(currentTemp+"\u00B0 F");
+      $('#humidity').text("Humidity: "+currentHumidity+"%");
+      $('#wind').text("Wind Speed: "+windSpeed.toFixed(2)+"MPH");
+      
+      //add city to city search history and re-save to localstorage
+      cityHistory = updateArray(cityHistory, currentCity);
+      localStorage.setItem('storedHistory', JSON.stringify(cityHistory));
+      updateHistoryUL(cityHistory);
+
   });
 }
 
@@ -72,32 +113,9 @@ $("#search-button").on('click',function(event){
   event.preventDefault();
   let queryCity = $('input').val();
   
-  
-    currentConditions = response;
-    currentCity = currentConditions.name;
-    
-    //get current weather information
-    let currentTemp = currentConditions.main.temp;
-    currentTemp = kelvToFar(currentTemp);
-    let currentHumidity = currentConditions.main.humidity;
-    let windSpeed = currentConditions.wind.speed*2.237;
-    
-    //save search as current city, save in local storage, and update html
-    localStorage.setItem('storedCity', JSON.stringify(currentCity));
-    $('#current-city').text(currentCity);
-    $('#temp').text(currentTemp+"\u00B0 F");
-    $('#humidity').text("Humidity: "+currentHumidity+"%");
-    $('#wind').text("Wind Speed: "+windSpeed.toFixed(2)+"MPH");
-    
-    //add city to city search history and re-save to localstorage
-    cityHistory.push(currentCity);
-    localStorage.setItem('storedHistory', JSON.stringify(cityHistory));
-    
-  });
+  getWeatherUpdateHTML(queryCity);
+
+
 });//search button onclick
 
-function clearLocalStorage(){
-  localStorage.setItem('storedCity', "");
-  localStorage.setItem('storedHistory', []);
-}
-
+getWeatherUpdateHTML(currentCity);
